155981035a8bb4992897cb0fdfbcb5c0cb8ea958
Chinedu Arinze <c.s.arinze@gmail.com>
2019-07-30 14:27:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,6 +19,11 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    @op = params[:operation]
 
+    switch @op
+    case add
+      (params[:number1].to_i + params[:number2].to_i).to_s
+    end
   end
 end
